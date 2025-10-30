c3276c29d9ec8acd41df8f5077709abb27da11f6
Михаил <st.mihaile87@gmail.com>
2025-10-30 15:48:12 +0400

    change rendering

---
+++ index.tsx
@@ -0,0 +1,11 @@
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from '../homework-1/src/app/App'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,6 @@ import { PostList } from '../widgets/PostList/PostList';
 const App = () => {
   return (
         <MainLayout>
-          <h2>Привет</h2>
       <PostList />
     </MainLayout>
   );
